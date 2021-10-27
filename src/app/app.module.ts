1105ab9f422666024d347a7c3e26a3425f7ac99e
Victor <v.mellaq@gmail.com>
2021-10-27 18:27:03 -0300

    Avance en estructura de carpetas y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ListadoNoticiasComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
