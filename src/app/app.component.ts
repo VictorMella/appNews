adbe1bc44b50f5c5953b2115c70802b48aa72a3a
Victor <v.mellaq@gmail.com>
2021-10-28 10:52:54 -0300

    Avances en estructura del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'appNews';
+  titulo: string;
+
+  constructor() {
+    this.titulo = 'Buscador de noticias'
+  }
+
+  ngOnInit() {
+
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './components/spinner/spinner.component';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 
 
 @NgModule({
   declarations: [SpinnerComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NgSelectModule
+  ],
+  exports: [
+    CommonModule,
+    NgSelectModule,
+    SpinnerComponent
+  ],
 })
 export class SharedModule { }
